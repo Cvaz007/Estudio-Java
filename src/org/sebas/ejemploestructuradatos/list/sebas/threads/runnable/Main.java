4a1e480fc2685233fdbc94db3a299cc2e095cfd2
Sebastian <sebastianmoreno301177@correo.itm.edu.co>
2023-08-20 21:33:17 -0500

    hilos son sleep

--- src/org/sebas/ejemploestructuradatos/list/sebas/threads/runnable/Main.java
+++ src/org/sebas/ejemploestructuradatos/list/sebas/threads/runnable/Main.java
@@ -2,9 +2,42 @@ package org.sebas.ejemploestructuradatos.list.sebas.threads.runnable;
 
 public class Main {
     public static void main(String[] args) {
-        new Thread( new ViajeTarea("Isla Malaga")).start();
-        new Thread( new ViajeTarea("Isla Margarita")).start();
-        new Thread( new ViajeTarea("Isla Demonio")).start();
+        //una manera de hacer lo mismo que tenemos en la clase viaje con run es:
+        Runnable viaje = new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    System.out.println(i + " - " + Thread.currentThread().getName());
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                System.out.println("Me voy a: " + Thread.currentThread().getName());
+            }
+        };
+        /*
+         Para hacer con funciones landa se escribe:
+         Runnable viaje = ()-<{
+         for (int i = 0; i < 10; i++) {
+                    System.out.println(i + " - " + Thread.currentThread().getName());
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                System.out.println("Me voy a: " + Thread.currentThread().getName());
+          }
+        */
+
+        new Thread(viaje, "Isla ").start();
+        new Thread(viaje, "Isla Pichu").start();
+
+        new Thread(new ViajeTarea("Isla Malaga")).start();
+        new Thread(new ViajeTarea("Isla Margarita")).start();
+        new Thread(new ViajeTarea("Isla Demonio")).start();
 
     }
 }
